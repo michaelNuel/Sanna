420d3fea5c521ed1dd34d2dd2167ef1679684bdc
michaelNuel <michaeltochukwuemmanuel45@gmail.com>
2024-10-30 09:37:30 +0100

    fixed something

--- app/item/[itemId]/page.tsx
+++ app/item/[itemId]/page.tsx
@@ -4,15 +4,19 @@ import ItemsHeroPage from "@/components/ItemsPage/ItemsHeroPage/ItemsHeroPage";
 import WhyDonate from "@/components/ItemsPage/WhyDonate/WhyDonate";
 import React from "react";
 
+// interface ItemProps {
+//   params: {
+//     itemId: string;
+//   };
+// }
 interface ItemProps {
-  params: {
+  params: Promise<{
     itemId: string;
-  };
+  }>;
 }
 
 const Item = async ({ params }: ItemProps) => {
   // Ensure you await if params are async
-  const { itemId } = await params;
 
   return (
     <div className="h-full w-full bg-gradient-to-r from-[#43305d] to-[#06030A]">
